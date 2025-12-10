3efeda7e24ef5d39647d61aab612afd1be554361
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-12-10 15:01:47 +0100

    update drizzle schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,21 @@
-import {betterAuth} from "better-auth";
-import {drizzleAdapter} from "better-auth/adapters/drizzle";
-import {db} from "@/lib/db/drizzle"; // Change l'import en fonction de TON
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import * as schema from "@/lib/db/schema"; // Change l'import en fonction
-
+import { db } from "@/lib/db/drizzle";
+import * as authSchema from "@/auth-schema";
 import {nextCookies} from "better-auth/next-js";
 export const auth = betterAuth({
- emailAndPassword: {
- enabled: true, // On active les comptes par email et mot de passe
- },
- database: drizzleAdapter(db, {
- provider: "pg",
- schema, // Ajoute ton schéma de DB
- }),
- plugins: [nextCookies()], // ⚠Permet de sauvegarder les cookies
-
+  database: drizzleAdapter(db, {
+    provider: "pg",
+   schema: authSchema,
+  }),
+  
+  user: {
+    modelName: "users", 
+  },
+  
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
 });
