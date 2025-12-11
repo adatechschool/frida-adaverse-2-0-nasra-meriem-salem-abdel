122abde04d91ff9923c20a926c6f31f8f3f98abc
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-12-11 11:06:05 +0100

    add components headers

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link"
+
+
 
 
 export const Header = () => {
     return (
-        <header>
-            <h1>AdaMarket marketplace</h1>
+        <header className="flex flex-col items-center gap-4 mb-10   border-b border-zinc-600 px-4 py-4 md:flex-row md:items-center md:px-10">
+            <Link href={"/"} 
+            className=" m-10 text-3xl font-semibold">
+            AdaMarketPlace
+            </Link>
+            <div className="flex gap-4 md:ml-auto md:gap-8">
+                <Link href={"/auth/sign-up"}
+                className="font-semibold">
+                S'inscrire
+                </Link>
+                <Link href={"/auth/sign-in"}
+                className="font-semibold">
+                Se connecter
+                </Link>
+            </div>
         </header>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { signout } from "./actions/connect";
+import { Header } from "./components/header";
 
 export default async function Home() {
     const session = await auth.api.getSession({ headers: await headers() });
 
   return (
     <div>
-      <pre>
-        {session ? JSON.stringify(session.user, null, 2) : "Not connected"}
-      </pre>
-      <button className="bg-white text-black" onClick={signout}>logout</button>
-    </div>
+      <Header></Header>
+    <pre>
+      {session ? JSON.stringify(session.user, null, 2) : "Not connected"}
+    </pre>
+    {session && (
+      <form action={signout}>
+        <button type="submit" className="bg-white text-black">
+          Logout
+        </button>
+      </form>
+    )}
+  </div>
   );
 }
