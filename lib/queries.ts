764841631219be6fa110dd0645ff62ced56bd2ba
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-12-15 14:18:58 +0100

    product by category

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,7 +1,7 @@
 import {db} from "@/lib/db/drizzle";
 import { categories,products,comments } from "./db/schema";
 
-import { eq } from "drizzle-orm";
+import { eq, ilike } from "drizzle-orm";
 
 export const getAllProducts = async () => {
   const ProductDb = await db.select().from(products)
@@ -23,4 +23,35 @@ export const getProductsById = async (id:number)=> {
   .where(eq(products.id, id))
 
   return result[0]   
+}
+
+
+
+
+export const getProductsByCategorySlug = async (slug: string) => {
+  const result = await db
+    .select({
+      id: products.id,
+      title: products.title,
+      slug: products.slug,
+      priceCents: products.priceCents,
+      imageUrl: products.imageUrl,
+    })
+    .from(products)
+    .innerJoin(
+      categories,
+      eq(products.categoryId, categories.id)
+    )
+    .where(ilike(categories.slug, slug));
+
+  return result;
+};
+
+export const getCategorieBySlug = async(slug:string)=>{ 
+  const result = await db
+    .select()
+    .from(categories)
+    .where(ilike(categories.slug, slug)); // ✅ comme ton getProductsByCategorySlug
+
+  return result[0]; // ✅ une seule catégorie
 }
