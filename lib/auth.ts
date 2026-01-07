9837050df86f605b7f732a72c7d2a96c21f660ab
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-01-07 11:43:47 +0100

    add base url

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db/drizzle";
 import * as authSchema from "@/lib/db/auth-schema";
 import {nextCookies} from "better-auth/next-js";
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL, // ✅ AJOUTE ÇA
   database: drizzleAdapter(db, {
     provider: "pg",
    schema: authSchema,
