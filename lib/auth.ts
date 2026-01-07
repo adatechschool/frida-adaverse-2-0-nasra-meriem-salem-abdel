7847154ebea5f32bf1de0b73cbe9e960c34ec9ba
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-01-07 12:00:57 +0100

    change url

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,8 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db/drizzle";
 import * as authSchema from "@/lib/db/auth-schema";
 import {nextCookies} from "better-auth/next-js";
+
+// console.log(process.env.BETTER_AUTH_URL)
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL, // ✅ AJOUTE ÇA
   database: drizzleAdapter(db, {
